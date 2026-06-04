8e8c2a5adfaa461e56f6118bf48ca5339a7be7bc
Hongbing Kou <hongbing@gmail.com>
2026-06-03 22:38:26 -0700

    Add: evaluate-reverse-polish-notation - submission-1

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String t : tokens) {
+            if ("+".equals(t)) {
+                int value1 = stack.pop();
+                int value2 = stack.pop();
+                stack.push(value2 + value1);
+            } else if ("-".equals(t)) {
+                int value1 = stack.pop();
+                int value2 = stack.pop();
+                stack.push(value2 - value1);
+            } else if ("*".equals(t)) {
+                int value1 = stack.pop();
+                int value2 = stack.pop();
+                stack.push(value1 * value2);
+            } else if ("/".equals(t)) {
+                int value1 = stack.pop();
+                int value2 = stack.pop();
+                stack.push(value2 / value1);
+            } else {
+                stack.push(Integer.parseInt(t));
+            }
+        }
+
+        return stack.peek();
+    }
+}
