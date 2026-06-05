21c714c3160fcf7c63c6827781bb4a84e2d85e49
Hongbing Kou <hongbing@gmail.com>
2026-06-05 09:39:17 -0700

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.java
@@ -0,0 +1,49 @@
+class TimeMap {
+    private class Datum {
+        private String value;
+        private int timestamp;
+
+        public Datum(String value, int timestamp) {
+            this.value = value;
+            this.timestamp = timestamp;
+        }
+    }
+
+    private Map<String, List<Datum>> dataMap;
+
+    public TimeMap() {
+        this.dataMap = new HashMap<>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        if (!dataMap.containsKey(key)) {
+            dataMap.put(key, new ArrayList<>());
+        }
+
+        dataMap.get(key).add(new Datum(value, timestamp));
+    }
+    
+    public String get(String key, int timestamp) {
+        if (!dataMap.containsKey(key)) {
+            return "";
+        }
+
+        List<Datum> dataList = dataMap.get(key);
+        int low = 0, high = dataList.size() - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            Datum midDatum = dataList.get(mid);
+            if (midDatum.timestamp == timestamp) {
+                return midDatum.value;
+            }
+
+            if (midDatum.timestamp < timestamp) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        return low < dataList.size() ? dataList.get(low).value : "";
+    }
+}
