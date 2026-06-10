4cd9fd5911f3c3191e707f0e1e14c18cd0b24ba6
Hongbing Kou <hongbing@gmail.com>
2026-06-09 23:47:15 -0700

    Add: design-twitter-feed - submission-5

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-5.java
@@ -0,0 +1,69 @@
+class Twitter {
+    private Map<Integer, Integer> tweetMap;
+    private Map<Integer, List<Integer>> userTweets;
+    private Map<Integer, Set<Integer>> userFollows;
+
+    private int clock;
+
+    public Twitter() {
+        this.tweetMap = new HashMap<>();
+        this.userTweets = new HashMap<>();
+        this.userFollows = new HashMap<>();
+    }
+    
+    public void postTweet(int userId, int tweetId) {
+        tweetMap.put(tweetId, clock++);
+
+        userTweets.putIfAbsent(userId, new ArrayList<>());
+        userTweets.get(userId).add(tweetId);
+
+        // System.out.println("Added tweet " + tweetId);
+    }
+    
+    public List<Integer> getNewsFeed(int userId) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> tweetMap.get(a) - tweetMap.get(b));
+
+        // Add own tweets
+        userFollows.putIfAbsent(userId, new LinkedHashSet<>());
+        
+        Set<Integer> followees = userFollows.get(userId);
+        // Ensure self follow
+        followees.add(userId);
+
+        for (int user : followees) {
+            if (userTweets.containsKey(user)) {
+                for (int tweet : userTweets.get(user)) {
+                    pq.offer(tweet);
+                    if (pq.size() > 10) {
+                        pq.poll();
+                    }
+                }
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        while (!pq.isEmpty()) {
+            ans.add(0, pq.poll());
+        }
+
+        return ans;
+    }
+    
+    public void follow(int followerId, int followeeId) {
+        if (followerId == followeeId) {
+            return;
+        }
+
+        userFollows.putIfAbsent(followerId, new LinkedHashSet<>());
+        userFollows.get(followerId).add(followeeId);
+    }
+    
+    public void unfollow(int followerId, int followeeId) {
+        if (!userFollows.containsKey(followerId)) {
+            return;
+        }
+
+        Set<Integer> followeeSet = userFollows.get(followerId);
+        followeeSet.remove(followeeId);
+    }
+}
