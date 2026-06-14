727e1f4bb5951ad78a0c9e2d61d2786c81be05b1
Hongbing Kou <hongbing@gmail.com>
2026-06-13 23:59:13 -0700

    Add: word-ladder - submission-1

---
+++ Data Structures & Algorithms/word-ladder/submission-1.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> words = new HashSet<>(wordList);
+        if (!words.contains(endWord)) {
+            return 0;
+        }
+
+        Map<String, List<String>> parents = new HashMap<>();
+        for (String w : wordList) {
+            char[] chs = w.toCharArray();
+            for (int i = 0; i < chs.length; i++) {
+                char savedChar = chs[i];
+                chs[i] = '*';
+                String replaceWord = new String(chs);
+                parents.putIfAbsent(replaceWord, new ArrayList<>());
+                parents.get(replaceWord).add(w);
+                chs[i] = savedChar;
+            }
+        }
+
+        Set<String> visited = new HashSet<>();
+        Queue<String> q = new LinkedList<>();
+        q.offer(beginWord);
+        int res = 1;
+        while (!q.isEmpty()) {
+            int len = q.size();
+            for (int i = 0; i < len; i++) {
+                String word = q.poll();
+                visited.add(word);
+                if (endWord.equals(word)) {
+                    return res;
+                }
+
+                char[] chs = word.toCharArray();
+                for (int j = 0; j < chs.length; j++) {
+                    char savedChar = chs[j];
+                    chs[j] = '*';
+                    String wildWord = new String(chs);
+                    chs[j] = savedChar;
+                    if (!parents.containsKey(wildWord)) continue;
+                    for (String next : parents.get(wildWord)) {
+                        if (visited.contains(next)) continue;
+                        q.offer(next);
+                    }
+                }
+            }
+
+            res++;
+        } 
+
+        return 0;       
+    }
+}
