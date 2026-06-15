ab75b3015b6764edaba95e4baf707ed55e0ac12f
Hongbing Kou <hongbing@gmail.com>
2026-06-14 20:25:34 -0700

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    public int coinChange(int[] coins, int amount) {
+        int[] memo = new int[amount + 1];
+        Arrays.fill(memo, -1);
+        return dp(coins, memo, amount);
+    }
+
+    private int dp(int[] coins, int[] memo, int amount) {
+        if (amount == 0) {
+            return 0;
+        } else if (memo[amount] != -1) {
+            return memo[amount];
+        }
+
+        boolean canChange = false;
+        int minCoin = Integer.MAX_VALUE;
+        for (int c : coins) {
+            if (amount - c < 0) continue;
+            int sub = dp(coins, memo, amount - c);
+            if (sub != -1) {
+                minCoin = Math.min(minCoin, sub + 1);
+            }
+        }
+
+        if (minCoin == Integer.MAX_VALUE) {
+            return -1;
+        }
+
+        memo[amount] = minCoin;
+        return  minCoin;
+    }
+}
