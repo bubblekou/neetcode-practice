e121455ded20d57fcbc6d71c64d86d76b9315f9f
Hongbing Kou <hongbing@gmail.com>
2026-06-17 21:19:34 -0700

    Add: target-sum - submission-1

---
+++ Data Structures & Algorithms/target-sum/submission-1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int n = nums.length;
+
+        return dfs(nums, n, 0, target);
+    }
+
+    private int dfs(int[] nums, int n, int i, int target) {
+        if (i == n && target == 0) {
+            return 1;
+        }
+
+        if (i >= n) {
+            return 0;
+        }
+
+        int ans = dfs(nums, n, i + 1, target - nums[i]);
+        ans += dfs(nums, n, i + 1, target + nums[i]);
+
+        return ans;
+    }
+}
