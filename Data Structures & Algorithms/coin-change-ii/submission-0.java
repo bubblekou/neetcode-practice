f061d609a5d4c780eb824043fca69b5d8e79dc87
Hongbing Kou <hongbing@gmail.com>
2026-06-17 21:06:15 -0700

    Add: coin-change-ii - submission-0

---
+++ Data Structures & Algorithms/coin-change-ii/submission-0.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+        int[][] memo = new int[n][amount + 1];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(memo[i], -1);
+        }
+
+        return dp(coins, amount, 0, memo);
+    }
+
+    private int dp(int[] coins, int amount, int i, int[][] memo) {
+        if (amount == 0) {
+            return 1;
+        } else if (i == coins.length) {
+            return 0;
+        } 
+
+        if (memo[i][amount] != -1) {
+            return memo[i][amount];
+        }
+
+        int ans = dp(coins, amount, i + 1, memo);
+        int balance = amount;
+        while (balance >= coins[i]) {
+            balance -= coins[i];
+            ans += dp(coins, balance, i + 1, memo);
+        }
+
+        memo[i][amount] = ans;
+        return ans;
+    }
+
+}
