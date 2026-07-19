3490b0d96378e3280b9cf06ea2cc4d184e8e9c8f
Hongbing Kou <hongbing@gmail.com>
2026-07-18 17:13:04 -0700

    Add: partition-labels - submission-0

---
+++ Data Structures & Algorithms/partition-labels/submission-0.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        List<Integer> ans = new ArrayList<>();
+
+        Map<Character, Integer> charCount = new HashMap<>();
+        for (char ch : s.toCharArray()) {
+            charCount.put(ch, charCount.getOrDefault(ch, 0) + 1);
+        }
+
+        int start = 0;
+        Set<Character> charsWithLeftOver = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (charCount.get(ch) == 1) {
+                charCount.remove(ch);
+                charsWithLeftOver.remove(ch);
+            } else {
+                charCount.put(ch, charCount.get(ch) - 1);
+                charsWithLeftOver.add(ch);
+            }
+
+            if (charsWithLeftOver.isEmpty()) {
+                ans.add(i - start + 1);
+                start = i + 1;
+            }
+        }
+
+        if (start < s.length()) {
+            ans.add(s.length() - start);
+        }
+
+        return ans;
+    }
+}
