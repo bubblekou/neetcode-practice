3718cbee0b7b7dce3bda2321a8c12f6870a49c5d
Hongbing Kou <hongbing@gmail.com>
2026-07-19 19:57:37 -0700

    Add: single-number - submission-1

---
+++ Data Structures & Algorithms/single-number/submission-1.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int v = 0;
+        for (int i = 0; i < nums.length; i++) {
+            v ^= nums[i];
+        }
+
+        return v;
+    }
+}
