393bca1fea8d4126e9e874974e17e86c63ad519d
Hongbing Kou <hongbing@gmail.com>
2026-07-19 20:31:24 -0700

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
+
+        return (n + 1) * n / 2 - sum;
+    }
+}
